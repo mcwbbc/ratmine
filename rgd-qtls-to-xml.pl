e3e1b2a25ec1ddd889222eb6934766b521a63ae3
Andrew Vallejos <avallejos@mcw.edu>
2009-09-11 14:00:45 -0500

    Updated QTL to include Symbol and a synonym Updated QTL to inlcude publication data Updated QTL to use Symnol in keyword search

--- rgd-qtls-to-xml.pl
+++ rgd-qtls-to-xml.pl
@@ -21,7 +21,7 @@ my ($model_file, $qtls_file, $gff_file) = @ARGV;
 die "Must point to valid InterMine Model" unless (-e $model_file);
 my $data_source = 'RGD';
 my $taxon_id = 10116;
-
+my $writer = new XML::Writer(DATA_MODE => 1, DATA_INDENT => 3);
 
 my @items = ();
 my %pubs = ();
@@ -33,6 +33,7 @@ my @gff;
 # valid classnames and fields
 my $model = new InterMine::Model(file => $model_file);
 my $item_factory = new InterMine::ItemFactory(model => $model);
+$writer->startTag("items");
 
 ####
 #User Additions
@@ -62,17 +63,25 @@ while(<QTLS>)
 		$qtl_item->set('organism', $org_item);
 		$qtl_item->set('primaryIdentifier', $qtl_info[$index{QTL_RGD_ID}]);
 		$qtl_item->set('symbol', $qtl_info[$index{QTL_SYMBOL}]);
+		
+		my $syn_item = $item_factory->make_item('Synonym');
+		$syn_item->set('value', $qtl_info[$index{QTL_SYMBOL}]);
+		$syn_item->set('type', 'symbol');
+		$syn_item->set('subject', $qtl_item);
+		$syn_item->as_xml($writer);
+		
 		$qtl_item->set('lod', $qtl_info[$index{LOD}]) unless $qtl_info[$index{LOD}] eq '';
 		$qtl_item->set('pValue', $qtl_info[$index{P_VALUE}]) unless $qtl_info[$index{P_VALUE}] eq '';
 		$qtl_item->set('trait', $qtl_info[$index{TRAIT_NAME}]);
 		$qtl_item->set('name', $qtl_info[$index{QTL_NAME}]);
+		$qtl_item->set('synonyms', [$syn_item]);
 		
 		unless($qtl_info[$index{'3.4_MAP_POS_START'}] eq '')
 		{
 			my @gff_line; #Create a GFF3 compatable line for each record
 			push(@gff_line, $qtl_info[$index{CHROMOSOME_FROM_REF}]); #chromsome location
 			push(@gff_line, "RatGenomeDatabase"); #source
-			push(@gff_line, "region"); #SOFA term
+			push(@gff_line, "QTL"); #SO term
 			push(@gff_line, $qtl_info[$index{'3.4_MAP_POS_START'}]); #start position
 			push(@gff_line, $qtl_info[$index{'3.4_MAP_POS_STOP'}]); #stop position
 			push(@gff_line, '.'); #score, left blank since QTLs have two different scores
@@ -82,6 +91,26 @@ while(<QTLS>)
 		
 			push(@gff, join("\t", @gff_line)); #add line to gff list
 		}
+		
+		if ($qtl_info[$index{CURATED_REF_PUBMED_ID}] ne '') {
+	      	my @publication_info = split(/;/, $qtl_info[$index{CURATED_REF_PUBMED_ID}]);
+	      	my @currentPubs = ();
+	      	foreach (@publication_info) {
+	        #reuse publication object if we already have it in the $pubs array
+	        	if (exists $pubs{$_}) {
+	          		push(@currentPubs, $pubs{$_});
+	        	}
+	        	#otherwise, create a new one via the item factory and add it to the $pubs array
+	        	else {
+	          		my $pub1 = $item_factory->make_item("Publication");
+	          		$pub1->set("pubMedId", $_);
+	          		$pubs{$_} = $pub1;
+	          		push(@currentPubs, $pub1);
+	        	}#end if-else
+	      	}#end foreach
+	      	$qtl_item->set("publications", \@currentPubs);
+    	}#end if
+
 		push(@items, $qtl_item);
 	} #end if-else	
 
@@ -89,16 +118,16 @@ while(<QTLS>)
 close QTLS;
 
 # write everything out as xml:
-my $writer = new XML::Writer(DATA_MODE => 1, DATA_INDENT => 3);
-$writer->startTag("items");
+
+
 #write the organism and the genes
 for my $item (@items) {
   $item->as_xml($writer);
 }
 #write the pubs
-#for my $item (values(%pubs)) {
-#  $item->as_xml($writer);
-#}
+for my $item (values(%pubs)) {
+  $item->as_xml($writer);
+}
 $writer->endTag("items");
 
 open(GFF, ">$gff_file");
