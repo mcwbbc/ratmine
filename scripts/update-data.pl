3055234d513482390e01e2539e0cfb29db02a23a
Andrew Vallejos <avallejos@mcw.edu>
2010-03-29 13:12:59 -0500

    created datadown script that will automatically update source data

---
+++ scripts/update-data.pl
@@ -0,0 +1,181 @@
+#!/usr/bin/perl
+#by Andrew Vallejos
+
+=head1 update-data.pl
+
+=begin text
+
+# perl update-data.pl
+#
+# Purpose:
+#	Download and preprocess, if necessary, data for InterMine build
+#
+#	Options:
+#	--conf=file\tconfiguration xml file
+#	--project=file\project.xml file
+
+=cut
+
+use XML::XPath;
+use Getopt::Long;
+use LWP::UserAgent;
+use strict;
+
+my ($conf, $project, $help, $verbose);
+
+GetOptions( 'config=s' => \$conf,
+			'project=s' => \$project,
+			'verbose' => \$verbose,
+			'help' => \$help );
+			
+if ($help or !($conf and $project))
+{
+	&printHelp;
+	exit(0);
+}
+
+my $projectInfo = &getProjectInfo($project);
+
+#foreach my $x (keys %$projectInfo) { print "$x : $$projectInfo{$x}\n"; }
+
+&updateData($conf, $projectInfo);
+
+exit (0);
+###Subroutines###
+
+sub printHelp
+{
+	print<<HELP
+
+ perl update-data.pl
+
+ Purpose:
+	Download and preprocess, if necessary, data for InterMine build
+
+	Options:
+	--conf=file\tconfiguration xml file
+	--project=file\tproject.xml file
+HELP
+}
+
+sub getProjectInfo
+{
+	my $project_file = shift;
+	
+	my $xp = XML::XPath->new(filename => $project_file);
+	my $nodeset = $xp->find('/project/sources/source'); #find all sources
+	
+	my %project_info;
+	foreach my $node ($nodeset->get_nodelist)
+	{
+		my $type = $node->find('@name')->string_value;
+		my $data;
+		if($data = $node->find('property[@name="src.data.file"]/@location')->string_value)
+		{	$project_info{$type}{file} = $data;	}
+		elsif($data = $node->find('property[@name="src.data.dir"]/@location')->string_value)
+		{	$project_info{$type}{dir} = $data;	}
+	}
+	
+	return \%project_info;
+}
+
+sub updateData
+{
+	my ($conf, $projectInfo) = @_;
+	
+	my $xp = XML::XPath->new(filename => $conf);
+	my $model = $xp->find('/project-config/model-file')->string_value;
+	
+	my $nodeset = $xp->find('/project-config/source');
+	
+	foreach my $node ($nodeset->get_nodelist)
+	{
+		my $source = $node->find('@name')->string_value;
+		my $remote = $node->find('remote-file')->string_value;
+		my $destination = $node->find('destination')->string_value;
+		my $localfile = '';
+		if ($remote)
+		{
+			if($$projectInfo{$source}{dir} or !$destination)
+			{
+				if($$projectInfo{$source}{file})
+				{
+					$destination = $$projectInfo{$source}{file};
+				}
+				elsif($$projectInfo{$source}{dir})
+				{
+					$localfile = $& if $remote =~ m|\/[^\/\\]+?$|;
+					$destination = $$projectInfo{$source}{dir} . $localfile if !$destination;
+				}
+			}#end if(!$destination)
+			
+			&downloadFile($remote, $destination);
+		}#end if ($remote)
+		
+		my $scriptset = $node->find('scripts/script');
+		foreach my $scriptnode ($scriptset->get_nodelist)
+		{
+			&runScript($scriptnode, $model, $destination, $source, $localfile);
+		}
+	}#end foreach $node
+}#end updateData
+
+sub runScript
+{
+	my($node, $model, $destination, $source, $localfile) = @_;
+	
+	my $script = $node->find('@script')->string_value;
+	if ($script)
+	{
+		my $options = '';
+		my $optionset = $node->find('option');
+		foreach my $opt ($optionset->get_nodelist)
+		{
+			my $type = $opt->find('@type')->string_value;
+			my $arguement = $opt->find('@name')->string_value;
+			$options .="--$arguement" if $arguement;
+			if ($type eq 'custom')
+			{
+				my $value = $opt->string_value;
+				$options .= " $value ";
+			}
+			elsif ($type eq 'flag')
+			{	$options .= ' ';  	} 
+			elsif($type eq 'model')
+			{
+				$options .= " $model ";
+			}
+			elsif($type eq 'input')
+			{
+				$options .= " $destination ";
+			}
+			elsif($type eq 'output')
+			{
+				$$projectInfo{$source}{file} ne '' ? 
+					$options .= " $$projectInfo{$source}{file} " :
+					$options .= " $$projectInfo{$source}{dir}$localfile ";
+			}
+		}#end foreach
+	
+		print "$script $options\n" if $verbose;
+		my $results =  `$script $options`;
+		print $results if $verbose;
+	}#end if ($script)
+}
+
+sub downloadFile
+{
+	my ($remoteFile, $localFile) = @_;
+	
+	my $ua = LWP::UserAgent->new;
+	my $req = HTTP::Request->new(GET => $remoteFile);
+	my $res = $ua->request($req, $localFile);
+	if ($res->is_success)
+	{
+		print "$remoteFile downloaded\n" if $verbose;
+	}
+	else
+	{
+		print $res->status_line . "\n";
+	}
+}
